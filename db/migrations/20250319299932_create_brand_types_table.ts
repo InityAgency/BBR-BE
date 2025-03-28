8028321f3515a575749f9a5d8ee330df3cc53540
alexandar993 <forfunnystuffs@gmail.com>
2025-03-28 21:59:29 +0100

    update brand type create table remove unique for name

--- db/migrations/20250319299932_create_brand_types_table.ts
+++ db/migrations/20250319299932_create_brand_types_table.ts
@@ -3,7 +3,7 @@ import type { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('brand_types', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-    table.string('name').notNullable().unique();
+    table.string('name').notNullable();
     table.text('description').nullable();
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
     table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
