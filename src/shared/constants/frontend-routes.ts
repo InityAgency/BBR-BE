44000a4998995868f7bb63cfbed287c1b90da068
alexandar993 <forfunnystuffs@gmail.com>
2025-05-09 01:28:26 +0200

    fix: route for verify email

--- src/shared/constants/frontend-routes.ts
+++ src/shared/constants/frontend-routes.ts
@@ -1,5 +1,5 @@
 export const FrontendRoutes = {
-  VERIFY_EMAIL: '/auth/verify-email',
+  VERIFY_EMAIL: '/verify-email',
   RESET_PASSWORD: '/auth/reset-password',
   INVITE_USER: '/auth/invite-user',
 };
