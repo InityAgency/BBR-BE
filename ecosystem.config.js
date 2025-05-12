47033db9cbe66fc5a6c004fb3395f9dad1dd1432
alexandar993 <forfunnystuffs@gmail.com>
2025-05-13 01:19:09 +0200

    remove ecocode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,10 +8,6 @@ module.exports = {
         instances: 1,
         autorestart: true,
         watch: false,
-        env: {
-          NODE_ENV: "production",
-          PORT: 3000,
-        },
       },
     ],
   };
